b6a33742f00cc917adba587725f0d005b93d955e
stillgood96 <a787574@naver.com>
2022-04-15 16:56:20 +0900

    return setting and connect mysql

--- Server.js
+++ Server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 5006;
 
+const aiGolf = require('./mysql/Db');
+
+
+/* localhost:5006 기본 접속시 Get */ 
 app.get('/', (req, res) => {
+
+    /* 서버접속 확인 */
     res.json({
         success: true,
     });
+
+    /* DB 연결 확인*/
+    aiGolf.connect(function(err) {
+        if(err) throw err;
+        console.log("connected !");
+    })
+
+
+    
 });
 
 app.listen(port, () => {
-    console.log(`server is listening at localhost:${process.env.PORT}`);
+    console.log(`서버가 켜졌따`);
 });
--- mysql/Db.js
+++ mysql/Db.js
@@ -1,9 +1,17 @@
 const mysql = require('mysql');  //
 
-const connection = mysql.createConnection({
+module.exports = mysql.createConnection({
      host:'127.0.0.1', 
      user:'root', 
      password:'aa787574!', 
      port:3306, 
      database:'aiGolf' 
     });
+
+/*
+con.connect(function(err) {
+     if(err) throw err;
+     console.log("connected !");
+})
+*/
+
