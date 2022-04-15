5052af649d876b518dd0c84c94c1a8c1b004a151
stillgood96 <a787574@naver.com>
2022-04-15 16:19:08 +0900

    add mysql and db information

---
+++ mysql/Db.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql');  //
+
+const connection = mysql.createConnection({
+     host:'127.0.0.1', 
+     user:'root', 
+     password:'aa787574!', 
+     port:3306, 
+     database:'aiGolf' 
+    });
